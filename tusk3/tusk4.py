2ee5964f3ea2677c83a7ccb8f67bf7be20fb8383
Evgeni.ok14 <evgeni.ok@mail.ru>
2023-01-28 17:40:37 +0300

    create a new project: tusk4

---
+++ tusk3/tusk4.py
@@ -0,0 +1,7 @@
+totalNumber = int(input('Введите общее количество журавликов: '))
+sergeiNumber = int((totalNumber / 3) / 2)
+petrNumber = sergeiNumber
+katiaNumber = int((petrNumber + sergeiNumber) *2)
+print ('Петя сделал ', petrNumber, 'количество журавликов ')
+print ('Сергей сделал ', sergeiNumber, 'количество журавликов ')
+print ('Катя сделала ', katiaNumber, 'количество журавликов ')
